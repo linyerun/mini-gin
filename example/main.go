12e18cf0e71ade0707ace11b09d02f786fe73776
Ernie <2338244917@qq.com>
2023-09-18 21:09:04 +0800

    去除logrus日志框架, 使用log标准库打印日志, 新增example代码

---
+++ example/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"github.com/linyerun/Gee/gee"
+	"github.com/linyerun/Gee/iface"
+	"html/template"
+	"time"
+)
+
+func main() {
+	e := gee.Default()
+
+	// 普通路由方法的使用
+	e.GET("/lin", func(c iface.IContext) {
+		c.JSON(200, gee.H{"msg": "GET method,ok!", "code": 200})
+	})
+	e.POST("/ye", func(c iface.IContext) {
+		c.JSON(200, gee.H{"msg": "POST method,ok!", "code": 200})
+	})
+
+	// 加载HTML文件的位置, 配合c.HTML使用
+	e.SetFuncMap(template.FuncMap{"FormatAsDate": FormatAsDate}) //设置参数要在加载模板前进行
+	e.LoadHTMLGlob("templates/*")
+	e.GET("/html", func(c iface.IContext) {
+		c.HTML(200, "format_as_date.html", gee.H{
+			"title": "gee",
+			"now":   time.Date(2023, 06, 22, 16, 20, 0, 0, time.UTC),
+		})
+	})
+
+	// 加载静态文件
+	e.Static("/assets", "./static")
+
+	// 使用中间件
+	e.Use(new(MyMiddleware))
+
+	// 使用路由分组功能
+	userGroup := e.Group("/user")
+	{
+		userGroup.GET("/lin", func(c iface.IContext) {
+			c.JSON(200, gee.H{"msg": "GET method,ok!", "code": 200})
+		})
+		userGroup.POST("/ye", func(c iface.IContext) {
+			c.JSON(200, gee.H{"msg": "POST method,ok!", "code": 200})
+		})
+		userGroup.Static("/assets", "./user_file")
+		e.Use(new(MyMiddleware))
+	}
+
+	// 运行
+	panic(e.Run(":8080"))
+}
+
+func FormatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
+}
+
+type MyMiddleware struct {
+}
+
+func (m *MyMiddleware) PrevHandle(c iface.IContext) {
+	fmt.Println(c.GetPath()+"执行前时间:", time.Now().Format("2006-01-02 15-04-05"))
+}
+
+func (m *MyMiddleware) LastHandle(c iface.IContext) {
+	fmt.Println(c.GetPath()+"执行后时间:", time.Now().Format("2006-01-02 15-04-05"))
+}
--- go.mod
+++ go.mod
@@ -1,7 +1,3 @@
 module github.com/linyerun/Gee
 
 go 1.18
-
-require github.com/sirupsen/logrus v1.9.0
-
-require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
